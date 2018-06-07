09408de1b5f77cace0b3c8e831c9d145e0067587
ahulley <ahulley@callibrity.com>
2018-06-07 16:15:46 -0400

    initial commit

--- blog/src/index.js
+++ blog/src/index.js
@@ -1,28 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
-import { createStore, applyMiddleware } from "redux";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import promise from "redux-promise";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 
-import reducers from "./reducers";
-import PostsIndex from "./components/posts_index";
-import PostsNew from "./components/posts_new";
-import PostsShow from "./components/posts_show";
+import App from './components/app';
+import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
+const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <BrowserRouter>
-      <div>
-        <Switch>
-          <Route path="/posts/new" component={PostsNew} />
-          <Route path="/posts/:id" component={PostsShow} />
-          <Route path="/" component={PostsIndex} />
-        </Switch>
-      </div>
-    </BrowserRouter>
-  </Provider>,
-  document.querySelector(".container")
-);
+    <App />
+  </Provider>
+  , document.querySelector('.container'));
